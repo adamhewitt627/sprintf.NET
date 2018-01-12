ade5c4441178420fda83c3004308c195de14ec5b
Adam Hewitt <adamhewitt@olivetree.com>
2018-01-12 15:42:28 -0800

    Initial project commit

---
+++ sprintf.NET.Tests/IntegerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SprintfNET.Tests
+{
+    [TestClass]
+    public class IntegerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ sprintf.NET/StringFormatter.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SprintfNET
+{
+    public static class StringFormatter
+    {
+    }
+}
