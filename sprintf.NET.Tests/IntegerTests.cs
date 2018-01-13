4752ceb5afd380441cb173a8f9e08181333661dc
Adam Hewitt <adamhewitt@olivetree.com>
2018-01-12 16:54:30 -0800

    Add basic formatting & start tests

--- sprintf.NET.Tests/IntegerTests.cs
+++ sprintf.NET.Tests/IntegerTests.cs
@@ -1,13 +1,46 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static SprintfNET.StringFormatter;
 
 namespace SprintfNET.Tests
 {
     [TestClass]
     public class IntegerTests
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        [DataTestMethod]
+        [DataRow(1, "1")]
+        [DataRow(42, "42")]
+        [DataRow(-81, "-81")]
+        [DataRow(int.MaxValue, "2147483647")]
+        [DataRow(int.MinValue, "-2147483648")]
+        public void Format(int value, string expected) 
+            => Assert.AreEqual(expected, PrintF("%d", value));
+
+        [DataTestMethod]
+        [DataRow(1, 3, "  1")]
+        [DataRow(42, -4, "42  ")]
+        [DataRow(-81, -8, "-81     ")]
+        [DataRow(-3, 5, "   -3")]
+        [DataRow(int.MaxValue, 2, "2147483647")]
+        [DataRow(int.MinValue, 4, "-2147483648")]
+        public void Padding(int value, int padding, string expected) 
+            => Assert.AreEqual(expected, PrintF($"%{padding}d", value));
+
+        [DataTestMethod]
+        [DataRow(1, 3, "001")]
+        [DataRow(-3, 5, "-0003")]
+        [DataRow(int.MaxValue, 2, "2147483647")]
+        [DataRow(int.MinValue, 4, "-2147483648")]
+        public void ZeroPadding(int value, int padding, string expected)
+            => Assert.AreEqual(expected, PrintF($"%0{padding}d", value));
+
+        [DataTestMethod]
+        [DataRow(1, "1")]
+        [DataRow(42, "42")]
+        [DataRow(-81, "-81")]
+        [DataRow(int.MaxValue, "2147483647")]
+        [DataRow(int.MinValue, "-2147483648")]
+        public void NSStyleFormat(int value, string expected)
+            => Assert.AreEqual(expected, PrintF("%@", value));
+
     }
 }
--- sprintf.NET/StringFormatter.cs
+++ sprintf.NET/StringFormatter.cs
@@ -1,8 +1,118 @@
 ﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace SprintfNET
 {
     public static class StringFormatter
     {
+        private const string PARAMETER = "parameter";
+        private const string FLAGS = "flags";
+        private const string WIDTH = "width";
+        private const string PRECISION = "precision";
+        private const string LENGTH = "length";
+        private const string TYPE = "type";
+        private const string STRING_REPLACE_PATTERN = @"\%"
+                                              + @"((?<" + PARAMETER + @">\d*)\$)?"
+                                              + @"(?<" + FLAGS + @">[\'\#\-\+ 0]+)?"
+                                              + @"(?<" + WIDTH + @">\d+)?"
+                                              + @"(\.(?<" + PRECISION + @">\d+))?"
+                                              + @"(?<" + LENGTH + @">[hl]l?)?"
+                                              + @"(?<" + TYPE + @">[dioxXucCsfeEgGpn%@])";
+
+        private static readonly Regex STRING_FORMAT_REGEX = new Regex(STRING_REPLACE_PATTERN, RegexOptions.ExplicitCapture);
+
+        public static string PrintF(string format, params object[] args)
+        {
+            if (format == null) throw new ArgumentNullException(nameof(format));
+
+            var argIndex = 0;
+            return STRING_FORMAT_REGEX.Replace(format, match =>
+            {
+                switch (match.Value)
+                {
+                    case "%%":
+                        return "%";
+
+                    default:
+                        var @param = match.Groups[PARAMETER];
+                        var index = @param.Success
+                            ? int.Parse(@param.Value) - 1
+                            : argIndex++;
+
+                        //Format string with the parameter stripped
+                        var fmt = string.Join(string.Empty, "%",
+                            match.Groups[FLAGS],
+                            match.Groups[WIDTH],
+                            match.Groups[PRECISION].Success ? "." + match.Groups[PRECISION] : string.Empty,
+                            match.Groups[LENGTH],
+                            match.Groups[TYPE]);
+
+                        return swprintf(fmt, args[index]); //?? "";
+                }
+            });
+        }
+
+        #region Native format
+        [DllImport("msvcrt", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int swprintf_s(string result, int maxLength, string format, int value);
+        [DllImport("msvcrt", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int swprintf_s(string result, int maxLength, string format, uint value);
+        [DllImport("msvcrt", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int swprintf_s(string result, int maxLength, string format, long value);
+        [DllImport("msvcrt", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int swprintf_s(string result, int maxLength, string format, ulong value);
+        [DllImport("msvcrt", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int _snwprintf_s(string result, int maxLength, int count, string format, double value);
+        [DllImport("msvcrt", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int swprintf_s(string result, int maxLength, string format, double value);
+        private static readonly Lazy<Func<string, int, string, double, int>> @double = new Lazy<Func<string, int, string, double, int>>(() => {
+            try
+            {
+                var i = _snwprintf_s(new string('\0', 8), 8, 8, "%f", 0);
+                return (r, l, f, a) => _snwprintf_s(r, l, l, f, a);
+            }
+            catch { return swprintf_s; }
+        });
+        [DllImport("msvcrt", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int swprintf_s(string result, int maxLength, string format, char value);
+
+        private static string swprintf(string format, object arg)
+        {
+            if (arg is string s) return s;
+            if (format == "%@") return string.Format(CultureInfo.InvariantCulture.NumberFormat, "{0}", arg);
+
+            int res = 0;
+            int size = 8;
+            string buffer = null;
+            const int maxSize = 256;
+            var formatter = buildFunc();
+
+            do
+            {
+                buffer = new string('\0', size);
+                res = formatter();
+            } while (res < 0 && (size *= 2) <= maxSize);
+
+            var result = buffer.Substring(0, res);
+            return result;
+
+            Func<int> buildFunc()
+            {
+                switch (arg)
+                {
+                    case int i: return () => swprintf_s(buffer, size, format, i);
+                    case uint i: return () => swprintf_s(buffer, size, format, i);
+                    case long i: return () => swprintf_s(buffer, size, format, i);
+                    case ulong i: return () => swprintf_s(buffer, size, format, i);
+                    case double i: return () => swprintf_s(buffer, size, format, i);
+                    case float i: return () => @double.Value(buffer, size, format, i);
+                    case char i: return () => swprintf_s(buffer, size, format, i);
+                    default: throw new ArgumentException($"Unsupported format argument: {arg} - Type: {arg?.GetType()}");
+                }
+            }
+        }
+        #endregion
     }
 }
