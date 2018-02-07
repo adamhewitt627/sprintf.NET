022b8ec838b43b57146b741ff18a2d3f1b30755e
Adam Hewitt <adamhewitt@olivetree.com>
2018-02-07 08:53:15 -0800

    See if a UWP lib build will pass WACK

---
+++ sprintf-native/buildNative.ps1
@@ -0,0 +1,30 @@
+try {
+    Push-Location $PSScriptRoot
+
+    @("x86", "x64", "ARM") | ForEach-Object {
+        $props = @{
+            Configuration = "Release"
+            Platform = $_
+            Keyword = "WindowsRuntimeComponent"
+            AppContainerApplication = "true"
+            ApplicationType = "`"Windows Store`""
+            OutDir = "..\sprintf.NET\runtimes\win-$($_.ToLower())\lib\uap10.0\"
+        }
+        msbuild -t:Clean,Build -p:(($props.GetEnumerator() | ForEach-Object { "$($_.Key)=$($_.Value)" }) -join ";")
+
+        Get-ChildItem $props.OutDir -Recurse  -File | ForEach-Object { 
+            $dest = Join-Path $_.Directory.Parent.FullName $_.Name
+            Move-Item $_.FullName -Destination $dest -Force
+        }
+        Remove-Item "$($props.OutDir)\sprintf-native"
+    }
+
+    @("x86", "x64"<#, "ARM"#>) | ForEach-Object {
+        $props = @{ 
+            Keyword = "Win32Proj"
+            Platform = $_
+            OutDir = "..\sprintf.NET\runtimes\win-$($_.ToLower())\lib\netstandard1.4\"
+        }
+        msbuild -t:Clean,Build -p:(($props.GetEnumerator() | ForEach-Object { "$($_.Key)=$($_.Value)" }) -join ";")
+    }
+} finally { Pop-Location }
