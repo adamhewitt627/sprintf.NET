18a67e149ced7bf6f5439b72884f877d28397a7a
Adam Hewitt <adamhewitt@olivetree.com>
2018-02-07 07:16:52 -0800

    Matching old UWP StringHelper

--- sprintf.NET/sprintf-native.cpp
+++ sprintf.NET/sprintf-native.cpp
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#include <stdio.h>
+#include "StrSafe.h"
 #include "stdint.h"
 
 #define FORMAT(type, suffix) extern "C" __declspec(dllexport) size_t Format##suffix(wchar_t* buffer, size_t length, wchar_t* format, type value) { \
